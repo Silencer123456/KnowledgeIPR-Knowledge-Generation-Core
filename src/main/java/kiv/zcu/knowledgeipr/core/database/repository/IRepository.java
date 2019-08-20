2850bcea8ee7b6d59f1d74af44595fdd80ebaa7b
silencer123456 <Hyku84jahy>
2019-08-20 14:21:33 +0200

    Changed repository interface back

--- src/main/java/kiv/zcu/knowledgeipr/core/database/repository/IRepository.java
+++ src/main/java/kiv/zcu/knowledgeipr/core/database/repository/IRepository.java
@@ -2,7 +2,6 @@ package kiv.zcu.knowledgeipr.core.database.repository;
 
 import kiv.zcu.knowledgeipr.core.database.specification.Specification;
 
-import java.sql.Connection;
 import java.util.List;
 
 // TODO: !!!CHANGE connection parameters
@@ -13,19 +12,19 @@ public interface IRepository<T> {
      * @param item - item to be inserted
      * @return id of the inserted record
      */
-    long add(Connection connection, T item);
+    long add(T item);
 
-    long add(Connection connection, Iterable<T> items);
+    long add(Iterable<T> items);
 
-    void update(Connection connection, T item);
+    void update(T item);
 
-    void remove(Connection connection, T item);
+    void remove(T item);
 
-    void remove(Connection connection, Specification specification);
+    void remove(Specification specification);
 
-    void removeAll(Connection connection);
+    void removeAll();
 
     T getById(int id);
 
-    List<T> query(Connection connection, Specification specification);
+    List<T> query(Specification specification);
 }
