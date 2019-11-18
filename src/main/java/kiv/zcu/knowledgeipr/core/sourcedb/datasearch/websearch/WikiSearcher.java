743b31cd0d83abd04e625e2929716b31e708c9fd
silencer123456 <Hyku84jahy>
2019-11-18 13:38:13 +0100

    - Wikipedia summaries are now longer and it is possible to search more results

--- src/main/java/kiv/zcu/knowledgeipr/core/sourcedb/datasearch/websearch/WikiSearcher.java
+++ src/main/java/kiv/zcu/knowledgeipr/core/sourcedb/datasearch/websearch/WikiSearcher.java
@@ -3,6 +3,7 @@ package kiv.zcu.knowledgeipr.core.sourcedb.datasearch.websearch;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.*;
 import java.net.URL;
@@ -22,14 +23,38 @@ public class WikiSearcher {
         return JsonNodeFactory.instance.objectNode();
     }
 
+    // TODO: REFACTORING!!!!!!
     private static JsonNode readJsonFromUrl(String url) throws IOException {
+        String concrete;
+        String wikiUrl;
+
         try (InputStream is = new URL(url).openStream()) {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String jsonText = readAll(rd);
 
             ObjectMapper mapper = new ObjectMapper();
 
-            return mapper.readTree(jsonText);
+            JsonNode firstHitNode = mapper.readTree(jsonText).get(1);
+            if (firstHitNode.size() == 0) {
+                return JsonNodeFactory.instance.objectNode();
+            }
+
+            wikiUrl = mapper.readTree(jsonText).get(3).get(0).textValue();
+
+            concrete = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + URLEncoder.encode(firstHitNode.get(0).textValue(), "UTF8");
+
+        }
+
+        try (InputStream is = new URL(concrete).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            JsonNode node = mapper.readTree(jsonText);
+            ((ObjectNode) node).put("url", wikiUrl);
+
+            return node;
         }
     }
 
